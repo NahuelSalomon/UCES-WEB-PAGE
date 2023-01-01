b062c1416b6f209c39652f3bd50f17bb17df6a5f
NahuelSalomon <nahuelmdp@hotmail.com>
2023-01-01 14:59:28 -0300

    Progress about polls

--- src/app/components/subjects/subject-poll-modal/subject-poll-modal.component.ts
+++ src/app/components/subjects/subject-poll-modal/subject-poll-modal.component.ts
@@ -2,9 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Career } from 'src/app/models/career';
 import { Poll } from 'src/app/models/poll';
+import { PollAnswer } from 'src/app/models/poll-answer';
 import { PollResponseType } from 'src/app/models/poll-response-type';
+import { Professor } from 'src/app/models/professor';
 import { Subject } from 'src/app/models/subject';
 import { PollService } from 'src/app/services/poll.service';
+import { ProfessorService } from 'src/app/services/professor.service';
 
 @Component({
   selector: 'app-subject-poll-modal',
@@ -16,20 +19,53 @@ export class SubjectPollModalComponent implements OnInit {
   @Input() subject: Subject;
   poll: Poll;
   pollResponseType = PollResponseType;
+  professorList : Array<Professor>;
+  answersList : Array<PollAnswer>;
 
-  constructor(private route :ActivatedRoute, private pollService: PollService) { }
+  constructor(private route :ActivatedRoute, private pollService: PollService, private professorService: ProfessorService) { }
 
   ngOnInit(): void {
     this.pollService.getBySubjectId(this.subject.id)
       .then(pollResponse=>{
 
         this.poll = pollResponse;
-        console.log(this.poll);
+        
+        this.poll.questions.forEach(q=>
+        {
+          var pollAnswer : PollAnswer  = new PollAnswer();
+          pollAnswer.pollQuestion = q;
+          this.answersList.push(pollAnswer);
+        })
+
+        
+        
       })
       .catch(pollError=>{
 
       });
-       
-  }
+
+      this.professorService.getAll()
+      
+      .then(professorResponse =>{
+        this.professorList = professorResponse;
+        
+      })
+      .catch(professorError =>{});
+    }
+
+    isFildAvailableByQuestion(id: number) : boolean
+    {
+      console.log(id);
+      
+      return true;
+
+    }
+
+    isNoAnswer() : boolean
+    {
+      return true;
+
+    }
+    
 
 }
--- src/app/models/poll-answer.ts
+++ src/app/models/poll-answer.ts
@@ -1,2 +1,14 @@
+import { PollQuestion } from "./poll-question";
+import { Professor } from "./professor";
+
 export class PollAnswer {
+
+    id: number;
+    rating: number;
+    shortAnswer: string;
+    positiveAnswer: boolean;
+    professor: Professor;
+    pollQuestion: PollQuestion;
+
+
 }

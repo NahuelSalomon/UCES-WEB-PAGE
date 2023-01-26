2969ec922d84f49f4db2c877afe5ed1cda7b220d
NahuelSalomon <nahuelmdp@hotmail.com>
2023-01-25 21:11:25 -0300

    Some changes ofe polls

--- src/app/components/subjects/subject-poll-modal/subject-poll-modal.component.ts
+++ src/app/components/subjects/subject-poll-modal/subject-poll-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Career } from 'src/app/models/career';
 import { Poll } from 'src/app/models/poll';
@@ -21,27 +22,44 @@ export class SubjectPollModalComponent implements OnInit {
   pollResponseType = PollResponseType;
   professorList : Array<Professor>;
   answersList : Array<PollAnswer>;
+  
+  pollQuestionFormGroup : FormGroup;
+
+  getFromControl(name: string) { return this.pollQuestionFormGroup.get(name);}
 
   constructor(private route :ActivatedRoute, private pollService: PollService, private professorService: ProfessorService) { }
 
   ngOnInit(): void {
+
+    this.pollQuestionFormGroup = new FormGroup({});
+    this.answersList = new Array<PollAnswer>();
+
     this.pollService.getBySubjectId(this.subject.id)
       .then(pollResponse=>{
 
         this.poll = pollResponse;
         
-        this.poll.questions.forEach(q=>
+        this.poll.questions.forEach(question=>
         {
           var pollAnswer : PollAnswer  = new PollAnswer();
-          pollAnswer.pollQuestion = q;
+          pollAnswer.pollQuestion = question;
+
+          var formControl : FormControl = new FormControl('', [ Validators.required ]);
+          formControl.disable(); 
+          this.pollQuestionFormGroup.addControl(question.pollResponseType + this.poll.id, formControl); 
+          
+
           this.answersList.push(pollAnswer);
         })
 
+
+        console.log(this.pollQuestionFormGroup);
         
-        
+
       })
       .catch(pollError=>{
-
+        console.log(pollError);
+        
       });
 
       this.professorService.getAll()
@@ -53,13 +71,6 @@ export class SubjectPollModalComponent implements OnInit {
       .catch(professorError =>{});
     }
 
-    isFildAvailableByQuestion(id: number) : boolean
-    {
-      console.log(id);
-      
-      return true;
-
-    }
 
     isNoAnswer() : boolean
     {
